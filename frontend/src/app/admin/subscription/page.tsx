4f24ac8ed47768ce2cc1bb597e5c6c0e25007abb
miyukitakahata <yamazakimiyuki0105@gmail.com>
2025-07-01 01:42:01 +0900

    stripe fetchURLをenvに設定

--- frontend/src/app/admin/subscription/page.tsx
+++ frontend/src/app/admin/subscription/page.tsx
@@ -32,7 +32,7 @@ export default function SubscriptionPage() {
     setLoading(true);
     try {
       const res = await fetch(
-        'http://localhost:8000/api/payments/create-checkout-session',
+        `${process.env.NEXT_PUBLIC_API_URL}/api/payments/create-checkout-session`,
         {
           method: 'POST',
         }
