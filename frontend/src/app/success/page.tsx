c97adcb47d5bc7bb5f1d67212680854045f3944f
miyukitakahata <yamazakimiyuki0105@gmail.com>
2025-07-01 09:18:16 +0900

    stripe導入

---
+++ frontend/src/app/success/page.tsx
@@ -0,0 +1,10 @@
+// src/app/success/page.tsx
+export default function SuccessPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold text-green-600">
+        決済が完了しました！ありがとうございます！
+      </h1>
+    </div>
+  );
+}
