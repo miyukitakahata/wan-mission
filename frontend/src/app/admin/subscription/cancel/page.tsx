45fe830f5494ce2c7d14db41f383d3a9764b7351
miyukitakahata <yamazakimiyuki0105@gmail.com>
2025-07-03 10:47:47 +0900

    firebase認証導入

--- frontend/src/app/admin/subscription/cancel/page.tsx
+++ frontend/src/app/admin/subscription/cancel/page.tsx
@@ -1,5 +1,11 @@
 // src/app/cancel/page.tsx
+import { useAuth } from '@/context/AuthContext'; // 認証情報を取得するカスタムフック
+
 export default function CancelPage() {
+  const user = useAuth(); // 認証情報を取得
+
+  console.log('[NamePage] User:', user.currentUser);
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <h1 className="text-2xl font-bold text-red-600">
--- frontend/src/app/admin/subscription/success/page.tsx
+++ frontend/src/app/admin/subscription/success/page.tsx
@@ -1,5 +1,11 @@
 // src/app/success/page.tsx
+import { useAuth } from '@/context/AuthContext'; // 認証情報を取得するカスタムフック
+
 export default function SuccessPage() {
+  const user = useAuth(); // 認証情報を取得
+
+  console.log('[NamePage] User:', user.currentUser);
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <h1 className="text-2xl font-bold text-green-600">
